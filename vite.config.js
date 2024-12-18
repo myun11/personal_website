508c8bb6bf55f653a6bbbd63e1661a14282d3c2c
Michael Yun <yun.michael.ds@gmail.com>
2024-12-18 13:24:36 -0500

    Added github pages config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.glb']
+  assetsInclude: ['**/*.glb'],
+  base: "https://michaelyun.dev/",
+  build: {
+    outDir: 'dist'
+  },
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  }
 })
