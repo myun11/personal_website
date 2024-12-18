cdfc883bd0a911abf91d5048e74cbbd0f2f7249e
Michael Yun <yun.michael.ds@gmail.com>
2024-12-18 13:04:05 -0500

    Started on mobile first feature

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,31 @@ import Navbar from './components/Navbar';
 const App = () => {
   
   return (
-    <main className='bg-slate-300/20 bg-vector bg-cover w-full h-screen relative'>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home2/>} />
-            {/*  */}
-          <Route path="/about" element={<About/>} /> 
-            {/* personal info, my timeline, undergrad, future prospects, current interests,*/}
-          <Route path="/projects" element={<Projects/>} />
-            {/* gallery of projects */}
-          <Route path="/contacts" element={<Contacts/>} />
-            {/* linkedin, github, email, insta */}
-        </Routes>
-      </Router>
+    <main className="bg-vector bg-cover w-screen h-screen relative">
+      <div className="">
+        <div className="flex flex-col justify-center items-start text-white px-8 py-4">
+          <h1 className="text-4xl font-bold">Michael Yun</h1>
+          <h2 className="text-xl font-medium mt-2">Full Stack Developer</h2>
+          <p className="text-base mt-4">
+            I enjoy building applications and optimizing workflow.
+          </p>
+        </div>
+        <div>
+          {/* <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home2/>} />
+                
+              <Route path="/about" element={<About/>} /> 
+                
+              <Route path="/projects" element={<Projects/>} />
+                
+              <Route path="/contacts" element={<Contacts/>} />
+                
+            </Routes>
+          </Router> */}
+        </div>
+      </div>
     </main>
   )
 }
