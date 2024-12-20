26e4dd6c52e7f2b7c706deb012bdef32e6f10f1f
Michael Yun <yun.michael.ds@gmail.com>
2024-12-20 15:22:27 -0500

    Added basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -10,14 +10,38 @@ import Navbar from './components/Navbar';
 const App = () => {
   
   return (
-    <main className="bg-slate-800 bg-cover w-screen h-screen relative">
+    <main className="bg-blue-950 bg-cover w-screen h-screen relative">
       <div className="">
-        <div className="flex flex-col justify-center items-start text-white px-8 py-4">
+        <div className="flex flex-col justify-center items-start text-gray-100 px-8 py-4">
           <h1 className="text-4xl font-bold">Michael Yun</h1>
           <h2 className="text-xl font-medium mt-2">Full Stack Developer</h2>
-          <p className="text-base mt-4">
-            I enjoy building applications and optimizing workflow. I specialize in React and AWS.
-          </p>
+          <h3 className="text-base text-gray-400 mt-4">
+            I like to solve problems.
+          </h3>
+        </div>
+        <div className ="flex flex-col justify-center items-start text-gray-100 px-8 py-4">
+          <h2 className="text-xl font-medium mt-2">About</h2>
+          <h3 className="text-base mt-4 text-gray-400 text-start">
+          I'm a dedicated US citizen software engineer with a strong focus on creating 
+          dynamic web applications and captivating data visualizations.
+          I specialize in designing visually stunning, user-friendly interfaces that deliver
+          exceptional user experiences.
+          With past experience optimizing workflows and managing end-to-end development,
+          I thrive on solving complex challenges. Driven by a constant hunger for learning
+          and improvement, I aim to bring innovation and excellence to every project I undertake.
+          </h3>
+        </div>
+
+        <div className ="flex flex-col justify-center items-start text-gray-100 px-8 py-4">
+          <h2 className="text-xl font-medium mt-2">Experience</h2>
+        </div>
+
+        <div className ="flex flex-col justify-center items-start text-gray-100 px-8 py-4">
+          <h2 className="text-xl font-medium mt-2">Projects</h2>
+        </div>
+
+        <div className ="flex flex-col justify-center items-start text-gray-100 px-8 py-4">
+          <h2 className="text-xl font-medium mt-2">Education</h2>
         </div>
         <div>
           {/* <Router>
